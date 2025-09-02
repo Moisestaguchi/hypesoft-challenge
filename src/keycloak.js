a6a91fabe02f7f1b41316e4392779cda54a07103
TaguchiMoises <devtaguchi@gmail.com>
2025-09-02 18:58:50 -0300

    chore(infra): remove and reconfigure frontend, adjust docker-compose

--- src/keycloak.js
+++
@@ -1,9 +0,0 @@
-import Keycloak from 'keycloak-js';
-
-const keycloak = new Keycloak({
-  url: 'http://localhost:8080', // URL do Keycloak
-  realm: 'AuthShop',           // o nome do Realm criada 
-  clientId: 'frontend-app',   // o Client ID 
-});
-
-export default keycloak;
